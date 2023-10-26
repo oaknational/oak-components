55acb1fe25c2d26060240ddd7892f23ee953470d
Simon Katan <simon.katan@thenational.academy>
2023-10-26 13:45:31 +0100

    feat: pa11y addon for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,8 +5,8 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
