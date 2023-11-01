7f10e5e77050a326e103080eb4625e2d7779b693
Simon Katan <simon.katan@thenational.academy>
2023-11-01 12:28:57 +0000

    feat: position style

---
+++ src/styles/utils/positionStyle.test.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import { positionStyle } from "./positionStyle";
+
+describe("positionStyle", () => {
+  test("should correctly handle prop 'position' as string", async () => {
+    const StyledComponent = styled.div`
+      ${positionStyle}
+    `;
+    const { getByTestId } = render(
+      <StyledComponent data-testid="test" $position="absolute" />,
+    );
+
+    expect(getByTestId("test")).toHaveStyle("position: absolute");
+  });
+
+  test("should correctly handle prop 'position' as array", async () => {
+    const StyledComponent = styled.div`
+      ${positionStyle}
+    `;
+    const { getByTestId } = render(
+      <StyledComponent data-testid="test" $position={["fixed"]} />,
+    );
+
+    expect(getByTestId("test")).toHaveStyle("position: fixed");
+  });
+
+  test.each([
+    ["$gap", "space-between-ssx", "gap: 0.5rem;"],
+    ["$top", "space-between-ssx", "top: 0.5rem;"],
+    ["$bottom", "space-between-ssx", "bottom: 0.5rem;"],
+    ["$left", "space-between-ssx", "left: 0.5rem;"],
+    ["$right", "space-between-ssx", "right: 0.5rem;"],
+    ["$overflow", "visible", "overflow: visible;"],
+    ["$overflowX", "visible", "overflow-x: visible;"],
+    ["$overflowY", "visible", "overflow-y: visible;"],
+  ])("should correctly handle props", (prop, value, expected) => {
+    const props = {
+      [prop]: value,
+    };
+
+    const StyledComponent = styled.div`
+      ${positionStyle}
+    `;
+    const { getByTestId } = render(
+      <StyledComponent data-testid="test" {...props} />,
+    );
+
+    expect(getByTestId("test")).toHaveStyle(expected);
+  });
+});
---
+++ src/styles/utils/positionStyle.ts
@@ -0,0 +1,41 @@
+import { CSSProperties } from "react";
+import { css } from "styled-components";
+
+import { responsiveStyle, ResponsiveValues } from "./responsiveStyle";
+
+import { OakAllSpacing, OakSpaceBetween } from "@/styles/theme/spacing";
+import { parseSpacing } from "@/styles/helpers/parseSpacing";
+
+type PositionSpacing = OakAllSpacing | OakSpaceBetween | null | undefined;
+
+export type PositionProps = {
+  $position?: ResponsiveValues<CSSProperties["position"]>;
+  $top?: ResponsiveValues<PositionSpacing>;
+  $right?: ResponsiveValues<PositionSpacing>;
+  $bottom?: ResponsiveValues<PositionSpacing>;
+  $left?: ResponsiveValues<PositionSpacing>;
+  $overflow?: ResponsiveValues<CSSProperties["overflow"]>;
+  $overflowX?: ResponsiveValues<CSSProperties["overflowX"]>;
+  $overflowY?: ResponsiveValues<CSSProperties["overflowY"]>;
+  $objectFit?: ResponsiveValues<CSSProperties["objectFit"]>;
+  $objectPosition?: ResponsiveValues<CSSProperties["objectPosition"]>;
+  $pointerEvents?: ResponsiveValues<CSSProperties["pointerEvents"]>;
+  $visibility?: ResponsiveValues<CSSProperties["visibility"]>;
+  $gap?: ResponsiveValues<PositionSpacing>;
+};
+
+export const positionStyle = css<PositionProps>`
+  ${responsiveStyle("position", (props) => props.$position)}
+  ${responsiveStyle("top", (props) => props.$top, parseSpacing)} 
+  ${responsiveStyle("right", (props) => props.$right, parseSpacing)} 
+  ${responsiveStyle("bottom", (props) => props.$bottom, parseSpacing)} 
+  ${responsiveStyle("left", (props) => props.$left, parseSpacing)} 
+  ${responsiveStyle("overflow", (props) => props.$overflow)}
+  ${responsiveStyle("overflow-x", (props) => props.$overflowX)}
+  ${responsiveStyle("overflow-y", (props) => props.$overflowY)}
+  ${responsiveStyle("object-fit", (props) => props.$objectFit)}
+  ${responsiveStyle("object-position", (props) => props.$objectPosition)}
+  ${responsiveStyle("pointer-events", (props) => props.$pointerEvents)}
+  ${responsiveStyle("visibility", (props) => props.$visibility)}
+  ${responsiveStyle("gap", (props) => props.$gap, parseSpacing)}
+`;
