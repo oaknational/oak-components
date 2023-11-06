d230e0a0e590f3750ae1eee9c3bf115bbc115a48
ben <ben.protheroe@thenational.academy>
2023-11-06 14:58:04 +0000

    chore(utils): relative imports and naming convention fix

--- src/styles/utils/borderStyle.test.tsx
+++ src/styles/utils/borderStyle.test.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-import { borderStyle } from "./borderStyle";
+import { borderStyle } from "@/styles/utils/borderStyle";
 
 import renderWithTheme from "@/test-helpers/renderWithTheme";
 
