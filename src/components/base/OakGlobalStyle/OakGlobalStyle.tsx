e7652a98a512997ae50f219895936ed107900804
Simon Katan <simon.katan@thenational.academy>
2023-11-07 14:15:10 +0000

    feat: add global styles to storybook

---
+++ src/components/base/OakGlobalStyle/OakGlobalStyle.tsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+import { resetStyles } from "@/styles/global/reset.styles";
+import { oakStyles } from "@/styles/global/oak.styles";
+
+/**
+ *
+ * Currently this is just for storybook as it is already applied in OWA
+ *
+ */
+
+export const OakGlobalStyle = createGlobalStyle`
+  ${resetStyles}
+  ${oakStyles}
+`;
---
+++ src/styles/global/oak.styles.ts
@@ -0,0 +1,61 @@
+/**
+ * This is our global stylesheet. It's for resets in addition to reset.styles.ts
+ * which is taken from a publically available reset. We want to minimise global
+ * styles where possible so if you add any styles here please comment with what
+ * they are doing and why they belong here (rather than in component specific
+ * styles)
+ */
+
+import { css } from "styled-components";
+
+export const oakStyles = css`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: Lexend, sans-serif;
+    font-weight: 300;
+    line-height: 1.4;
+  }
+
+  /* stylelint-disable */
+  body,
+  #__next {
+    /* stylelint-enable */
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+    font-family: inherit;
+
+    /*
+     * Remove grey background on tap on ios
+     */
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  button {
+    /*
+     * some browsers have a small margin by default
+     * @see https://github.com/oaknational/Oak-Web-Application/issues/709
+     */
+    margin: 0;
+
+    /*
+     * Remove grey background on tap on ios
+     */
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  /*
+  * Inline styling for mathjax equations
+  */
+  mjx-container {
+    display: inline-grid !important;
+    margin: 0 !important;
+  }
+`;
