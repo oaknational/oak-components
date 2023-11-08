c01a91b8778c345a8f9e79fbb75c60ed5cc341cc
ben <ben.protheroe@thenational.academy>
2023-11-08 15:14:22 +0000

    chore(OakP): add test coverage

---
+++ src/components/base/OakP/OakP.test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "@testing-library/jest-dom";
+import OakP from "./OakP";
+
+import renderWithTheme from "@/test-helpers/renderWithTheme";
+
+describe("P", () => {
+  test("should set the correct font-family", () => {
+    const { getByTestId } = renderWithTheme(
+      <OakP data-testid="paragraph">Here is some paragraph text</OakP>,
+    );
+    expect(getByTestId("paragraph")).toHaveStyle(
+      "font-family: Lexend,sans-serif",
+    );
+  });
+});
