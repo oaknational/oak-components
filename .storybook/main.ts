9bc070f7d9d493a901cb6a717a6f7da8f15c0785
Simon Katan <simon.katan@thenational.academy>
2023-11-08 17:38:34 +0000

    fix: attempt to get next envars working in storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,9 @@
 const config = {
+  env: (config) => ({
+    ...config,
+    NEXT_PUBLIC_OAK_ASSETS_HOST: process.env.NEXT_PUBLIC_OAK_ASSETS_HOST,
+    NEXT_PUBLIC_OAK_ASSETS_PATH: process.env.NEXT_PUBLIC_OAK_ASSETS_PATH,
+  }),
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
