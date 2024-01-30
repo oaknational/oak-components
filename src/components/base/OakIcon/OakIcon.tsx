cc1f5c30d9131dcb599799e77aae1255ba8f10fd
Carl Whittaker <carl359@gmail.com>
2024-01-30 15:12:30 +0000

    feat(PUPIL-282): add subject icons to `OakIcon`

--- src/components/base/OakIcon/OakIcon.tsx
+++ src/components/base/OakIcon/OakIcon.tsx
@@ -1,68 +1,17 @@
 import React from "react";
 
-import * as imageMap from "../../../image-map.json";
+import { IconName, icons } from "../../../image-map";
 
 import { OakImage, OakImageProps } from "@/components/base/OakImage";
 
-export const oakIconNames = [
-  "home",
-  "send",
-  "rocket",
-  "video",
-  "edit",
-  "hamburger",
-  "cross",
-  "bell",
-  "twitter",
-  "worksheet",
-  "facebook",
-  "share",
-  "arrow-right",
-  "worksheet-3",
-  "chevron-right",
-  "save",
-  "quiz-3",
-  "chevron-down",
-  "linkedin",
-  "magic-carpet",
-  "books",
-  "supervision-level",
-  "quiz-white",
-  "arrow-left",
-  "additional-material",
-  "slide-deck-3",
-  "sign-language",
-  "external",
-  "equipment-required",
-  "chevron-left",
-  "download",
-  "search",
-  "chevron-up",
-  "go",
-  "copyright",
-  "arrow-down",
-  "project",
-  "quiz",
-  "slide-deck",
-  "content-guidance",
-  "tick",
-  "instagram",
-  "dot",
-  "dot-png",
-  "warning",
-  "lightbulb",
-  "lightbulb-yellow",
-  "intro",
-] as const;
+export const oakIconNames = Object.keys(icons) as IconName[];
 
-export type OakIconName = (typeof oakIconNames)[number];
+export type OakIconName = IconName;
 
 export type OakIconProps = Omit<OakImageProps, "alt" | "src"> & {
   iconName: OakIconName;
   alt?: string;
 };
-
-const typedImageMap = imageMap as Record<string, Record<OakIconName, string>>;
 
 export const OakIcon = (props: OakIconProps) => {
   const {
@@ -74,13 +23,10 @@ export const OakIcon = (props: OakIconProps) => {
     ...rest
   } = props;
 
-  const _alt = alt ?? iconName;
-  const imagePath = typedImageMap?.["icons"]?.[iconName];
-
   return (
     <OakImage
-      src={`https://${process.env.NEXT_PUBLIC_OAK_ASSETS_HOST}/${process.env.NEXT_PUBLIC_OAK_ASSETS_PATH}/${imagePath}`}
-      alt={_alt}
+      src={`https://${process.env.NEXT_PUBLIC_OAK_ASSETS_HOST}/${process.env.NEXT_PUBLIC_OAK_ASSETS_PATH}/${icons[iconName]}`}
+      alt={alt ?? iconName}
       $width={$width}
       $height={$height}
       placeholder="empty"
