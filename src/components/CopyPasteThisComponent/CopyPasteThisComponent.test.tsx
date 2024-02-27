95c473dcdee7bb6a29d1812cf3ded3637e6f3580
ben <ben.protheroe@thenational.academy>
2024-02-27 12:03:27 +0000

    feat(copy-paste-this): create template component folder and files

---
+++ src/components/CopyPasteThisComponent/CopyPasteThisComponent.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { create } from "react-test-renderer";
+
+import { CopyPasteThisComponent } from "./CopyPasteThisComponent";
+
+import renderWithTheme from "@/test-helpers/renderWithTheme";
+
+describe("CopyPasteThisComponent", () => {
+  it("renders", () => {
+    const { getByTestId } = renderWithTheme(
+      <CopyPasteThisComponent data-testid="test" />,
+    );
+    expect(getByTestId("test")).toBeInTheDocument();
+  });
+
+  it("matches snapshot", () => {
+    const tree = create(
+      <CopyPasteThisComponent>Click Me</CopyPasteThisComponent>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
