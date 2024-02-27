1d544ad24d509daf5ad11c67a63a01baef619362
ben <ben.protheroe@thenational.academy>
2024-02-27 13:31:37 +0000

    feat(copy-paste-this): add js bock hints

--- src/components/CopyPasteThisComponent/CopyPasteThisComponent.tsx
+++ src/components/CopyPasteThisComponent/CopyPasteThisComponent.tsx
@@ -11,48 +11,62 @@ const StyledOakFlex = styled(OakFlex)`
 // For example you could restyle the OakFlex component by adding the styles to the css template literal below
 
 export type CopyPasteThisComponentProps = {
-  // Define the props for your component here
-  // add styleprops from "@/styles/utils/*Style" to the component props type definition where necessary in order to use
-  // the oak design kit which accept specific tokems to add style and also keep the components style cosistent
-  //  & TypographyStyleProps &
-  //   SpacingStyleProps &
-  //   ColorStyleProps &
-  //   DisplayStyleProps &
-  //   BorderStyleProps &
-  //   ColorFilterStyleProps &
-  //   DropShadowStyleProps &
-  //   FlexStyleProps &
-  //   ListStyleProps &
-  //   OpacityStyleProps &
-  //   PositionStyleProps &
-  //   SizeStyleProps &
-  //   TransformStyleProps &
-  //   TransitionStyleProps &
-  //   ZIndexStyleProps;
+  /**
+   * Define the props for your component here
+   * add styleprops from "@/styles/utils/*Style" to the component props type definition where necessary in order to use
+   * the oak design kit which accept specific tokens to add style and also keep the components style consistent
+   *  & TypographyStyleProps &
+   *   SpacingStyleProps &
+   *   ColorStyleProps &
+   *   DisplayStyleProps &
+   *   BorderStyleProps &
+   *   ColorFilterStyleProps &
+   *   DropShadowStyleProps &
+   *   FlexStyleProps &
+   *   ListStyleProps &
+   *   OpacityStyleProps &
+   *   PositionStyleProps &
+   *   SizeStyleProps &
+   *   TransformStyleProps &
+   *   TransitionStyleProps &
+   *   ZIndexStyleProps;
+   */
 };
 
 // By adding the style css utils to this components css your component will be able to accept corresponding props and prop values.
 // you can also add custom styles to the component by adding the styles to the css template literal below
 
 const CopyPasteThisComponentCss = css<CopyPasteThisComponentProps>``;
-// add default and custom styles to the component by adding the styles to the css template literal below
 
-//   ${typographyStyle}
-//   ${colorStyle}
-//   ${spacingStyle}
-//   ${displayStyle}
-//   ${borderStyle}
-//   ${dropShadowStyle}
-//  ${colorFilterStyle}
+/**
+ *
+ * add default and custom styles to the component by adding the styles to the css template literal below
+ *
+ * ${typographyStyle}
+ * ${colorStyle}
+ * ${spacingStyle}
+ * ${displayStyle}
+ * ${borderStyle}
+ * ${dropShadowStyle}
+ * ${colorFilterStyle}
+ *
+ */
 
 const UnstyledComponent = (props: CopyPasteThisComponentProps) => {
-  // Add your component logic here
+  /**
+   * Add your component logic here
+   *
+   */
 
   return (
-    // Return your component JSX here
-    // for example you could
+    /**
+     *  Return your component JSX here
+     * for example you could
+     *
+     */
+
     <StyledOakFlex {...props}>
-      {/* Add your component content here */}
+      {/** Add your component content here */}
     </StyledOakFlex>
   );
 };
