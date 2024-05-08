b9d4d22e9bc8eeb6214f247533bece84e27e3705
Simon Katan <simon.katan@thenational.academy>
2024-05-08 12:00:42 +0100

    docs: improve storybook entry

---
+++ src/components/organisms/pupil/OakPupilJourneyList/OakPupilJourneyList.test.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { create } from "react-test-renderer";
+
+import { OakPupilJourneyList } from "./OakPupilJourneyList";
+
+import renderWithTheme from "@/test-helpers/renderWithTheme";
+import { OakThemeProvider } from "@/components/atoms";
+import { oakDefaultTheme } from "@/styles";
+
+describe("OakPupilJourneyList", () => {
+  it("should render successfully", () => {
+    const { getByText } = renderWithTheme(
+      <OakPupilJourneyList phase="primary">
+        <p>Hello World</p>
+      </OakPupilJourneyList>,
+    );
+    expect(getByText("Hello World")).toBeInTheDocument();
+  });
+
+  it("matches snapshot", () => {
+    const tree = create(
+      <OakThemeProvider theme={oakDefaultTheme}>
+        <OakPupilJourneyList phase="primary">
+          <p>Hello World</p>
+        </OakPupilJourneyList>
+      </OakThemeProvider>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/components/organisms/pupil/OakPupilJourneyList/OakPupilJourneyList.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { OakFlex } from "@/components/atoms/OakFlex";
+
+export type OakPupilJourneyListProps = {
+  children: React.ReactNode;
+  phase: "primary" | "secondary";
+};
+
+/**
+ *
+ * A styled list container for use with OakPupilJourneyListItems
+ *
+ */
+
+export const OakPupilJourneyList = ({
+  children,
+  phase,
+}: OakPupilJourneyListProps) => {
+  const backgroundColor =
+    phase === "primary" ? "bg-decorative4-subdued" : "bg-decorative3-subdued";
+  return (
+    <OakFlex
+      $flexDirection={"column"}
+      $pa={"inner-padding-m"}
+      $background={backgroundColor}
+      $borderRadius={"border-radius-l"}
+      $gap={"space-between-s"}
+    >
+      {children}
+    </OakFlex>
+  );
+};
