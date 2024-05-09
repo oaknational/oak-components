23a699cc35967f48e1bab4fec3ec3380cf01867f
Kyle Huggins <kyle.huggins7@gmail.com>
2024-05-09 11:27:43 +0100

    chore: change color name

--- src/components/organisms/OakHeader/OakHeader.tsx
+++ src/components/organisms/OakHeader/OakHeader.tsx
@@ -1,7 +1,6 @@
 import React, { ReactNode } from "react";
 import styled, { css } from "styled-components";
 
-import { parseColor } from "@/styles/helpers/parseColor";
 import {
   OakFlex,
   OakP,
