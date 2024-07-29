6dd9bc9faf171c66e7eade539c7a314abd2343be
Kyle Huggins <kyle.huggins7@gmail.com>
2024-07-29 13:30:11 +0100

    chore: remove .only from tests

--- src/components/organisms/OakPagination/OakPagination.test.tsx
+++ src/components/organisms/OakPagination/OakPagination.test.tsx
@@ -33,7 +33,7 @@ describe("OakPagination Component", () => {
     expect(getAllByTestId("page-number-component")).toHaveLength(7);
   });
 
-  it.only("disables the backwards button when on the first page", () => {
+  it("disables the backwards button when on the first page", () => {
     const { getByTestId } = renderWithTheme(
       <OakPagination
         paginationHref={""}
@@ -45,7 +45,7 @@ describe("OakPagination Component", () => {
     expect(getByTestId("backwards-button")).toBeDisabled();
   });
 
-  it.only("disables the backwards button when on the first page", () => {
+  it("disables the backwards button when on the first page", () => {
     const { getByTestId } = renderWithTheme(
       <OakPagination
         paginationHref={""}
