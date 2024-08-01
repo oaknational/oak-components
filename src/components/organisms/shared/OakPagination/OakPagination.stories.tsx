26e0ceac87004e3f84f4b756027f50494b162208
Kyle Huggins <kyle.huggins7@gmail.com>
2024-08-01 18:07:57 +0100

    feat: mock out router so pagination component renders

--- src/components/organisms/shared/OakPagination/OakPagination.stories.tsx
+++ src/components/organisms/shared/OakPagination/OakPagination.stories.tsx
@@ -1,7 +1,37 @@
+import { ParsedUrlQuery } from "querystring";
+
 import React from "react";
 import { StoryObj, Meta } from "@storybook/react";
+import { NextRouter } from "@storybook/nextjs/router.mock";
 
 import { OakPagination } from "./OakPagination";
+
+const mockQuery: ParsedUrlQuery = {
+  page: "1",
+};
+const mockRouter: NextRouter = {
+  asPath: "/",
+  route: "/",
+  basePath: "",
+  isLocaleDomain: false,
+  pathname: "/",
+  query: mockQuery,
+  push: () => Promise.resolve(true),
+  replace: () => Promise.resolve(true),
+  reload: () => window.location.reload(),
+  back: () => window.history.back(),
+  prefetch: () => Promise.resolve(),
+  beforePopState: () => null,
+  events: {
+    on: () => null,
+    off: () => null,
+    emit: () => null,
+  },
+  forward: () => null,
+  isFallback: false,
+  isReady: true,
+  isPreview: false,
+};
 
 const meta: Meta<typeof OakPagination> = {
   title: "Components/organisms/shared/OakPagination",
@@ -13,6 +43,15 @@ const meta: Meta<typeof OakPagination> = {
     pageName: { type: "string" },
   },
   parameters: {
+    nextjs: {
+      router: {
+        asPath: "/",
+        pathname: "/",
+        query: {
+          page: 1,
+        },
+      },
+    },
     controls: {
       include: ["totalPages", "paginationHref", "pageName", "type"],
     },
@@ -28,7 +67,8 @@ export const Default: Story = {
   args: {
     totalPages: 7,
     currentPage: 1,
-    paginationHref: "#",
+    paginationHref: "/#",
     pageName: "test",
+    router: mockRouter,
   },
 };
