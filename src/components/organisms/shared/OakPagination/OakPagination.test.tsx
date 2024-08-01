5212c966e8841e19e7acd401266aad43d8e00554
Kyle Huggins <kyle.huggins7@gmail.com>
2024-08-01 19:51:20 +0100

    feat: add pagination test

--- src/components/organisms/shared/OakPagination/OakPagination.test.tsx
+++ src/components/organisms/shared/OakPagination/OakPagination.test.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { RefObject } from "react";
 import "@testing-library/jest-dom";
 import { create } from "react-test-renderer";
-import { NextRouter } from "next/router";
+import { NextRouter, useRouter } from "next/router";
+import { act, fireEvent } from "@testing-library/react";
 
 import { OakPagination } from "./OakPagination";
 import { generatePageNumbers } from "./utils";
@@ -35,8 +36,7 @@ const mockRouter: Partial<NextRouter> = {
 
 describe("OakPagination Component", () => {
   beforeEach(() => {
-    // Mock the useRouter hook to return the mockRouter
-    // (useRouter as jest.Mock).mockReturnValue(mockRouter);
+    jest.resetAllMocks();
   });
 
   it("renders", () => {
@@ -53,6 +53,21 @@ describe("OakPagination Component", () => {
       </OakThemeProvider>,
     );
     expect(getByTestId("pagination")).toBeInTheDocument();
+  });
+
+  it("should not render when no additional pages", () => {
+    const { queryByTestId } = renderWithTheme(
+      <OakThemeProvider theme={oakDefaultTheme}>
+        <OakPagination
+          paginationHref={""}
+          pageName={"test"}
+          currentPage={1}
+          totalPages={1}
+          router={mockRouter as NextRouter}
+        />
+      </OakThemeProvider>,
+    );
+    expect(queryByTestId("pagination")).not.toBeInTheDocument();
   });
 
   it("renders the correct number of pages", () => {
@@ -102,6 +117,132 @@ describe("OakPagination Component", () => {
     const forwardsButton = getByTestId("forwards-button");
 
     expect(forwardsButton).toBeDisabled();
+  });
+
+  it("should handle forward chevron click correctly", () => {
+    const paginationHref = "/test";
+    const shouldAppendQuery = true;
+
+    const { getByTestId } = renderWithTheme(
+      <OakPagination
+        totalPages={7}
+        paginationHref={paginationHref}
+        currentPage={1}
+        shouldAppendQuery={shouldAppendQuery}
+        router={mockRouter as NextRouter}
+        pageName={"test"}
+      />,
+    );
+
+    const forwardButton = getByTestId("forwards-button");
+    fireEvent.click(forwardButton);
+
+    expect(mockRouter.push).toHaveBeenCalledWith(
+      `${paginationHref}&page=2`,
+      undefined,
+      { shallow: true },
+    );
+  });
+
+  it("should handle backward chevron click correctly", () => {
+    const paginationHref = "/test";
+    const shouldAppendQuery = true;
+
+    const { getByTestId } = renderWithTheme(
+      <OakPagination
+        totalPages={7}
+        paginationHref={paginationHref}
+        currentPage={3}
+        shouldAppendQuery={shouldAppendQuery}
+        router={mockRouter as NextRouter}
+        pageName={"test"}
+      />,
+    );
+
+    const forwardButton = getByTestId("backwards-button");
+    fireEvent.click(forwardButton);
+
+    expect(mockRouter.push).toHaveBeenCalledWith(
+      `${paginationHref}&page=2`,
+      undefined,
+      { shallow: true },
+    );
+  });
+
+  it("should handle backward chevron click correctly", () => {
+    const paginationHref = "/test?search=test";
+    const shouldAppendQuery = true;
+
+    const { getAllByTestId } = renderWithTheme(
+      <OakPagination
+        totalPages={7}
+        paginationHref={paginationHref}
+        currentPage={4}
+        shouldAppendQuery={shouldAppendQuery}
+        router={mockRouter as NextRouter}
+        pageName={"test"}
+      />,
+    );
+
+    const numberButtons = getAllByTestId("page-number-component");
+
+    numberButtons.forEach((button, i) => {
+      const number = i + 1;
+      fireEvent.click(button);
+      expect(mockRouter.push).toHaveBeenCalledWith(
+        `${paginationHref}&page=${number}`,
+        undefined,
+        { shallow: true },
+      );
+    });
+  });
+
+  it.only("should focus the first item and scroll to top when router.query.page changes", () => {
+    const mockRouter = {
+      query: { page: "3" },
+      push: jest.fn(),
+    };
+
+    (useRouter as jest.Mock).mockReturnValue(mockRouter);
+
+    const focusMock = jest.fn();
+    const firstItemRef = {
+      current: { focus: focusMock },
+    } as unknown as RefObject<HTMLAnchorElement>;
+
+    const scrollToMock = jest.fn();
+    window.scrollTo = scrollToMock;
+
+    const { rerender } = renderWithTheme(
+      <OakPagination
+        totalPages={7}
+        paginationHref="/test"
+        currentPage={3}
+        shouldAppendQuery={true}
+        router={mockRouter as unknown as NextRouter}
+        pageName="test"
+        firstItemRef={firstItemRef}
+      />,
+    );
+
+    mockRouter.query.page = "2";
+
+    act(() => {
+      rerender(
+        <OakPagination
+          totalPages={7}
+          paginationHref="/test"
+          currentPage={2}
+          shouldAppendQuery={true}
+          router={mockRouter as unknown as NextRouter}
+          pageName="test"
+          firstItemRef={firstItemRef}
+        />,
+      );
+    });
+
+    expect(focusMock).toHaveBeenCalled();
+    expect(scrollToMock).toHaveBeenCalledWith({ top: 0, behavior: "smooth" });
   });
 
   it("matches snapshot", () => {
