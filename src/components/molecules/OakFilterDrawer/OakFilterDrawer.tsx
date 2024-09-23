7bf6eca4382e65687835ae5fe091094b0c496c76
Kyle Huggins <kyle.huggins7@gmail.com>
2024-09-23 16:19:10 +0100

    fix: type error with additional import

--- src/components/molecules/OakFilterDrawer/OakFilterDrawer.tsx
+++ src/components/molecules/OakFilterDrawer/OakFilterDrawer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import { FocusOn } from "react-focus-on";
 import { Transition, TransitionStatus } from "react-transition-group";
