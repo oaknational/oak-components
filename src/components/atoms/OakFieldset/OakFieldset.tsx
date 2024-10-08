21123176cdb2e3d32f2a8f4c39b1ad5c91bf4238
Kyle Huggins <kyle.huggins7@gmail.com>
2024-10-08 15:56:27 +0100

    fix: refactor fieldset component

--- src/components/atoms/OakFieldset/OakFieldset.tsx
+++ src/components/atoms/OakFieldset/OakFieldset.tsx
@@ -1,41 +1,14 @@
 import styled from "styled-components";
 
-import { colorStyle, ColorStyleProps } from "@/styles/utils/colorStyle";
-import { opacityStyle, OpacityStyleProps } from "@/styles/utils/opacityStyle";
-import {
-  marginStyle,
-  MarginStyleProps,
-  paddingStyle,
-  PaddingStyleProps,
-} from "@/styles/utils/spacingStyle";
-import {
-  typographyStyle,
-  TypographyStyleProps,
-} from "@/styles/utils/typographyStyle";
-import { borderStyle, BorderStyleProps } from "@/styles/utils/borderStyle";
-import { sizeStyle, SizeStyleProps } from "@/styles/utils/sizeStyle";
-
-export type OakFieldsetProps = ColorStyleProps &
-  SizeStyleProps &
-  OpacityStyleProps &
-  MarginStyleProps &
-  PaddingStyleProps &
-  BorderStyleProps &
-  TypographyStyleProps;
-
+import { OakBoxProps, oakBoxCss } from "@/components/atoms/OakBox";
+export type OakFieldsetProps = OakBoxProps;
 /**
  * OakFieldset renders a custom `fieldset` component, removes default styling of fieldset.
   color, opacity, margin, padding, border and typography styles can be passed in also.
  */
 export const OakFieldset = styled.fieldset<OakFieldsetProps>`
-  border: 0px;
+  border: 0;
   margin: 0;
   padding: 0;
-  ${colorStyle}
-  ${sizeStyle}
-  ${opacityStyle}
-  ${marginStyle}
-  ${paddingStyle}
-  ${borderStyle}
-  ${typographyStyle}
+  ${oakBoxCss};
 `;
