0d674da6fe6c49f57b1e7ec85b776408ec9b116b
ben <ben.protheroe@thenational.academy>
2024-11-11 19:59:37 +0000

    feat(button): create scale image button

--- src/components/molecules/OakScaleImageButton/OakScaleImageButton.stories.tsx
+++ src/components/molecules/OakScaleImageButton/OakScaleImageButton.stories.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Meta, StoryObj } from "@storybook/react";
 
 import { OakScaleImageButton } from "./OakScaleImageButton";
 
-import { OakCloudinaryImage, OakFlex } from "@/components/atoms";
+import { OakFlex } from "@/components/atoms";
 
 const meta: Meta<typeof OakScaleImageButton> = {
   component: OakScaleImageButton,
@@ -37,38 +37,4 @@ export const Default: Story = {
       <OakScaleImageButton {...args} />
     </OakFlex>
   ),
-};
-
-export const withImageScaleCallback: Story = {
-  render: (args) => {
-    const [scaled, setScale] = useState<boolean>(false);
-    return (
-      <OakFlex
-        $width={"all-spacing-7"}
-        $height={"all-spacing-7"}
-        $pointerEvents={"auto"}
-      >
-        <OakCloudinaryImage
-          alt={""}
-          cloudinaryId="v1705942058/test-images/Cat_August_2010-4_lklxsr.jpg"
-          width={scaled ? 7280 : 3640}
-          height={scaled ? 4452 : 2226}
-          $minWidth={scaled ? "all-spacing-22" : "all-spacing-11"}
-        />
-        <OakScaleImageButton
-          {...args}
-          onImageScaleCallback={() => {
-            setScale(!scaled);
-          }}
-          isExpanded={scaled}
-        />
-      </OakFlex>
-    );
-  },
-  args: {},
-  parameters: {
-    controls: {
-      include: [],
-    },
-  },
 };
