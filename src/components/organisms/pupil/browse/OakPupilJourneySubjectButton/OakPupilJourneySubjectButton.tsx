343c8a9449cf469768e3b97f64bdc7f7ecc971cc
Natassja Blore <natassja.blore@thenational.academy>
2025-04-25 15:04:08 +0100

    refactor: use test ids instead of alt text for tests

--- src/components/organisms/pupil/browse/OakPupilJourneySubjectButton/OakPupilJourneySubjectButton.tsx
+++ src/components/organisms/pupil/browse/OakPupilJourneySubjectButton/OakPupilJourneySubjectButton.tsx
@@ -72,7 +72,7 @@ export const OakPupilJourneySubjectButton = <C extends ElementType = "button">({
   const iconOverride = (
     <OakIcon
       iconName={subjectIconName}
-      alt={subjectIconName}
+      data-testid={subjectIconName}
       $minWidth={"all-spacing-12"}
       $minHeight={"all-spacing-12"}
       aria-hidden="true"
