e28bde29c9e61e9793f7f979c4e66a9f4e4153b9
Reece Charsville <reece@forward.digital>
2025-05-08 15:40:52 +0100

    fix: added role alert to error messages in storybook

--- src/components/organisms/teacher/OakTeacherNotesModal/OakTeacherNotesModal.stories.tsx
+++ src/components/organisms/teacher/OakTeacherNotesModal/OakTeacherNotesModal.stories.tsx
@@ -147,7 +147,7 @@ const getMsgs = () => {
     "Please do not include phone numbers. This information will be redacted.",
     "Please do not include addresses. This information will be redacted.",
   ].map((m) => (
-    <OakFlex $alignItems="center" $gap="space-between-ssx">
+    <OakFlex $alignItems="center" $gap="space-between-ssx" role="alert">
       <OakIcon iconName="error" $colorFilter="icon-error" />
       <OakSpan $color="icon-error" $font="body-3-bold">
         {m}
