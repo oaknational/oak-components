e653001b8fe540a7aee46e0d90357891e2a40ab0
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2025-06-16 16:36:51 +0100

    feat: add iconName and isTrailingIcon props to OakTagFunctional component, update storybook

--- src/components/molecules/OakTagFunctional/OakTagFunctional.stories.tsx
+++ src/components/molecules/OakTagFunctional/OakTagFunctional.stories.tsx
@@ -3,26 +3,54 @@ import { Meta, StoryObj } from "@storybook/react";
 
 import { OakTagFunctional } from "./OakTagFunctional";
 
-import { OakFlex } from "@/components/atoms";
+import { OakFlex, oakIconNames } from "@/components/atoms";
+const controlIconNames = [
+  null,
+  [...oakIconNames].sort((a, b) => a.localeCompare(b)),
+].flat();
 
 const meta: Meta<typeof OakTagFunctional> = {
   component: OakTagFunctional,
   tags: ["autodocs"],
   title: "components/molecules/OakTagFunctional",
-  args: { label: "Played" },
-  decorators: [
-    (Story) => (
-      <OakFlex $pa={"inner-padding-xl"} $flexDirection={"row"}>
-        <Story />
-      </OakFlex>
-    ),
-  ],
+  argTypes: {
+    label: { control: "text" },
+    iconName: { options: controlIconNames },
+    isTrailingIcon: { type: "boolean" },
+  },
+  parameters: {
+    controls: {
+      include: ["iconName", "label"],
+    },
+  },
 };
+
 export default meta;
 
 type Story = StoryObj<typeof OakTagFunctional>;
 
 export const Default: Story = {
-  render: (args) => <OakTagFunctional {...args} />,
+  render: (args) => {
+    const iconName = args.iconName ? args.iconName : "arrow-right";
+    return (
+      <OakFlex $gap="space-between-m">
+        <OakTagFunctional
+          label={args.label ? args.label : "No icon"}
+          $background="mint"
+        />
+        <OakTagFunctional
+          label={args.label ? args.label : "With icon"}
+          iconName={iconName}
+          $background="pink"
+        />
+        <OakTagFunctional
+          label={args.label ? args.label : "Trailing icon"}
+          iconName={iconName}
+          isTrailingIcon
+          $background="lavender"
+        />
+      </OakFlex>
+    );
+  },
   args: { $background: "bg-neutral", $color: "text-subdued" },
 };
--- src/components/molecules/OakTagFunctional/OakTagFunctional.tsx
+++ src/components/molecules/OakTagFunctional/OakTagFunctional.tsx
@@ -1,21 +1,42 @@
 import React from "react";
 
-import { OakBox, OakBoxProps, OakLabel } from "@/components/atoms";
+import {
+  OakBoxProps,
+  OakFlex,
+  OakIcon,
+  OakIconName,
+  OakLabel,
+} from "@/components/atoms";
 
 export type OakTagFunctionalProps = {
   label: string;
+  iconName?: OakIconName;
+  isTrailingIcon?: boolean;
 } & Omit<OakBoxProps, "onClick" | "label">;
 export const OakTagFunctional = (props: OakTagFunctionalProps) => {
   const { label, ...oakBoxProps } = props;
+  const icon = props.iconName && (
+    <OakIcon
+      $height={"all-spacing-5"}
+      $width={"all-spacing-5"}
+      iconName={props.iconName}
+    />
+  );
+
+  const trailingIcon = props.isTrailingIcon && icon;
+  const leadingIcon = !props.isTrailingIcon && icon;
   return (
-    <OakBox
+    <OakFlex
       $borderRadius={"border-radius-m"}
       $ph={"inner-padding-xs"}
       $pv={"inner-padding-ssx"}
       $font={["heading-light-7"]}
+      $gap={"space-between-ssx"}
       {...oakBoxProps}
     >
+      {leadingIcon}
       <OakLabel>{label}</OakLabel>
-    </OakBox>
+      {trailingIcon}
+    </OakFlex>
   );
 };
