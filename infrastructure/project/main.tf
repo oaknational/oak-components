7d3a328b92b9364207497c26327663fb1f7f1dc4
Kofoworola Ogunleye <kay16th@yahoo.com>
2025-06-20 07:56:15 -0400

    feat: add Vercel configuration using shared terraform module

---
+++ infrastructure/project/main.tf
@@ -0,0 +1,24 @@
+locals {
+  workspace_prefix = "components-project-"
+}
+
+resource "terraform_data" "workspace_validation" {
+  lifecycle {
+    precondition {
+      condition     = startswith(terraform.workspace, local.workspace_prefix)
+      error_message = "Workspace name \"${terraform.workspace}\" must begin with ${local.workspace_prefix}"
+    }
+  }
+}
+
+module "vercel" {
+  source                           = "github.com/oaknational/oak-terraform-modules//modules/vercel_project?ref=v1.1.0"
+  build_command                    = "npm run build-storybook"
+  build_type                       = "storybook"
+  environment_variables            = local.environment_variables
+  framework                        = "storybook"
+  deployment_type                  = "standard_protection"
+  git_repo                         = "oaknational/oak-components"
+  protection_bypass_for_automation = false
+  skew_protection                  = "1 day"
+}
