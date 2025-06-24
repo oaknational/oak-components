d75dc349d62e12e46827ab6190a5f2a6b8805c74
Kofoworola Ogunleye <kay16th@yahoo.com>
2025-06-24 11:21:29 -0400

    feat: create a tn.a domain for components
    - creates a domain for oak-components

--- infrastructure/project/main.tf
+++ infrastructure/project/main.tf
@@ -12,13 +12,16 @@ resource "terraform_data" "workspace_validation" {
 }
 
 module "vercel" {
-  source                           = "github.com/oaknational/oak-terraform-modules//modules/vercel_project?ref=v1.1.0"
+  source                           = "github.com/oaknational/oak-terraform-modules//modules/vercel_project?ref=v1.2.1"
   build_command                    = "npm run build-storybook"
   build_type                       = "storybook"
+  cloudflare_zone_domain           = var.cloudflare_zone_domain
   environment_variables            = local.environment_variables
   framework                        = "storybook"
   deployment_type                  = "standard_protection"
   git_repo                         = "oaknational/oak-components"
   protection_bypass_for_automation = false
   skew_protection                  = "1 day"
+
+  domains = ["components-vercel.thenational.academy"]
 }
