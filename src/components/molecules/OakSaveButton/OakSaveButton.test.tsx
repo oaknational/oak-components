f2ec1cca477c8b482120c2428e42865b909a9b1b
Natassja Blore <natassja.blore@thenational.academy>
2025-07-01 14:12:35 +0100

    refactor: extract save button to component

---
+++ src/components/molecules/OakSaveButton/OakSaveButton.test.tsx
@@ -0,0 +1,118 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { screen } from "@testing-library/react";
+
+import { OakSaveButton } from "./OakSaveButton";
+
+import renderWithTheme from "@/test-helpers/renderWithTheme";
+
+const render = renderWithTheme;
+
+describe("OakSaveButton", () => {
+  it("renders correctly with default props", () => {
+    render(
+      <OakSaveButton
+        isSaved={false}
+        isLoading={false}
+        onSave={() => console.log("Save action triggered")}
+        unavailable={false}
+        saveButtonId="save-button"
+        title="Test unit"
+      />,
+    );
+    const button = screen.getByRole("button", {
+      name: "Save this unit: Test unit",
+    });
+
+    expect(button).toBeInTheDocument();
+  });
+  it('renders correctly with "Saved" text when isSaved is true', () => {
+    render(
+      <OakSaveButton
+        isSaved={true}
+        isLoading={false}
+        onSave={() => console.log("Save action triggered")}
+        unavailable={false}
+        saveButtonId="save-button"
+        title="Test unit"
+      />,
+    );
+    const button = screen.getByRole("button", {
+      name: "Unsave this unit: Test unit",
+    });
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent("Saved");
+  });
+  it('renders correctly with "Save" text when isSaved is false', () => {
+    render(
+      <OakSaveButton
+        isSaved={false}
+        isLoading={false}
+        onSave={() => console.log("Save action triggered")}
+        unavailable={false}
+        saveButtonId="save-button"
+        title="Test unit"
+      />,
+    );
+    const button = screen.getByRole("button", {
+      name: "Save this unit: Test unit",
+    });
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent("Save");
+  });
+  it("disables the button when isLoading is true", () => {
+    render(
+      <OakSaveButton
+        isSaved={false}
+        isLoading={true}
+        onSave={() => console.log("Save action triggered")}
+        unavailable={false}
+        saveButtonId="save-button"
+        title="Test unit"
+      />,
+    );
+    const button = screen.getByRole("button", {
+      name: "Save this unit: Test unit",
+    });
+
+    expect(button).toBeDisabled();
+  });
+  it("disables the button when unavailable is true", () => {
+    render(
+      <OakSaveButton
+        isSaved={false}
+        isLoading={false}
+        onSave={() => console.log("Save action triggered")}
+        unavailable={true}
+        saveButtonId="save-button"
+        title="Test unit"
+      />,
+    );
+    const button = screen.getByRole("button", {
+      name: "Save this unit: Test unit",
+    });
+
+    expect(button).toBeDisabled();
+  });
+  it("calls onSave when the button is clicked", () => {
+    const mockOnSave = jest.fn();
+    render(
+      <OakSaveButton
+        isSaved={false}
+        isLoading={false}
+        onSave={mockOnSave}
+        unavailable={false}
+        saveButtonId="save-button"
+        title="Test unit"
+      />,
+    );
+    const button = screen.getByRole("button", {
+      name: "Save this unit: Test unit",
+    });
+
+    button.click();
+    expect(mockOnSave).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/components/molecules/OakSaveButton/OakSaveButton.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { OakSmallTertiaryInvertedButton } from "../OakSmallTertiaryInvertedButton";
+
+export type OakSaveButtonProps = {
+  isSaved: boolean;
+  isLoading: boolean;
+  onSave: () => void;
+  unavailable?: boolean;
+  saveButtonId?: string;
+  title: string;
+};
+
+export const OakSaveButton = (props: OakSaveButtonProps) => {
+  const { isSaved, isLoading, onSave, unavailable, saveButtonId, title } =
+    props;
+  return (
+    <OakSmallTertiaryInvertedButton
+      iconName={isSaved ? "bookmark-filled" : "bookmark-outlined"}
+      isTrailingIcon
+      disabled={isLoading || unavailable}
+      onClick={onSave}
+      width="all-spacing-15"
+      $justifyContent="end"
+      aria-label={`${isSaved ? "Unsave" : "Save"} this unit: ${title} `}
+      id={saveButtonId}
+    >
+      {isSaved ? "Saved" : "Save"}
+    </OakSmallTertiaryInvertedButton>
+  );
+};
