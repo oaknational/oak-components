e955a5619466d01d8a30371dbeee0f12627c0037
Jessica Ababio <jessicaababio12@gmail.com>
2025-09-16 15:18:52 +0100

    feat: add template files for OakMultilineText component

---
+++ src/components/atoms/OakMultilineText/OakMultilineText.stories.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { StoryObj, Meta } from "@storybook/react";
+
+import { OakMultilineText } from "./OakMultilineText";
+
+const meta: Meta<typeof OakMultilineText> = {
+  //  "title" is the title of the story and where to look for component in the storybook
+  title: "Components/OakMultilineText",
+  component: OakMultilineText,
+  tags: ["autodocs"],
+  argTypes: {
+    // Define your component's props and their types here
+    // For example:
+    // text: { control: "text" },
+    // size: { control: 'select', options: ['small', 'medium', 'large'] },
+    //
+    // can also use the storybook-helpers to add the argTypes
+    // ...colorArgTypes,
+    // ...spacingArgTypes,
+    // ...borderArgTypes,
+  },
+  parameters: {
+    controls: {
+      include: [
+        // include the argTypes from the storybook-helpers
+        // ...Object.keys(colorArgTypes),
+        // ...Object.keys(spacingArgTypes),
+        // ...Object.keys(borderArgTypes),
+        "type",
+      ],
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof OakMultilineText>;
+
+export const Default: Story = {
+  render: (args) => <OakMultilineText {...args} />,
+  args: {
+    // Define your component's default props here
+    //   $background: "bg-btn-primary",
+    //   $color: "white",
+    //   $ba: "border-solid-s",
+    //   $pa: "inner-padding-s",
+    //   $borderRadius: "border-radius-m",
+  },
+};
---
+++ src/components/atoms/OakMultilineText/OakMultilineText.test.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { create } from "react-test-renderer";
+
+import { OakMultilineText } from "./OakMultilineText";
+
+import renderWithTheme from "@/test-helpers/renderWithTheme";
+
+describe("OakMultilineText", () => {
+  it("renders", () => {
+    const { getByTestId } = renderWithTheme(
+      <OakMultilineText data-testid="test" />,
+    );
+    expect(getByTestId("test")).toBeInTheDocument();
+  });
+
+  it("matches snapshot", () => {
+    const tree = create(<OakMultilineText>Click Me</OakMultilineText>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
