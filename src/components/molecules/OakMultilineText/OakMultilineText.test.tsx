81d76624e2e9a34f6d51ba9877ccac2bb1240fe2
Jessica Ababio <jessicaababio12@gmail.com>
2025-09-24 11:39:49 +0100

    feat: update snapshot and add props to tests for OakMultilineText

--- src/components/molecules/OakMultilineText/OakMultilineText.test.tsx
+++ src/components/molecules/OakMultilineText/OakMultilineText.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { create } from "react-test-renderer";
 
 import { OakMultilineText } from "./OakMultilineText";
 
@@ -8,23 +7,25 @@ import renderWithTheme from "@/test-helpers/renderWithTheme";
 
 describe("OakMultilineText", () => {
   it("renders", () => {
-    const { getByTestId } = renderWithTheme(
+    const { getByPlaceholderText } = renderWithTheme(
       <OakMultilineText
-        data-testid="test"
-        $charLimit={200}
+        charLimit={200}
         $height="all-spacing-10"
+        placeholder="Start typing answer..."
+        disabled={false}
       />,
     );
-    expect(getByTestId("test")).toBeInTheDocument();
+    expect(getByPlaceholderText("Start typing answer...")).toBeInTheDocument();
   });
 
   it("matches snapshot", () => {
-    const tree = create(
+    const { container } = renderWithTheme(
       <OakMultilineText
-        $charLimit={200}
+        charLimit={200}
         $height="all-spacing-10"
+        disabled={false}
       ></OakMultilineText>,
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+    );
+    expect(container).toMatchSnapshot();
   });
 });
