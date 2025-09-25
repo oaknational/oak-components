f4fb64063289c0cada09e7ba72bf6493e182b659
Jessica Ababio <jessicaababio12@gmail.com>
2025-09-25 10:30:25 +0100

    feat: add new props, update styling and add invalid text p tag

--- src/components/molecules/OakMultilineText/OakMultilineText.tsx
+++ src/components/molecules/OakMultilineText/OakMultilineText.tsx
@@ -1,14 +1,12 @@
-import React, { SetStateAction, useState } from "react";
+import React, { useState, forwardRef } from "react";
 import styled from "styled-components";
 
 import { OakFlex } from "@/components/atoms/OakFlex";
 import { OakTextArea, OakTextAreaProps } from "@/components/atoms/OakTextArea";
-import { SizeStyleProps } from "@/styles/utils/sizeStyle";
-import { OakSpan } from "@/components/atoms";
+import { OakSpan, OakP } from "@/components/atoms";
 import { parseColor } from "@/styles/helpers/parseColor";
-import { parseBorderRadius } from "@/styles/helpers/parseBorderRadius";
-import { parseSpacing } from "@/styles/helpers/parseSpacing";
-import { parseBorderWidth } from "@/styles/helpers/parseBorderWidth";
+import { OakCombinedSpacingToken } from "@/styles";
+import { ResponsiveValues } from "@/styles/utils/responsiveStyle";
 
 // For example you could restyle the OakFlex component by adding the styles to the css template literal below
 
@@ -16,7 +14,7 @@ export type OakMultilineTextProps = {
   /**
    * Height of component
    */
-  $height: SizeStyleProps["$height"];
+  $height: ResponsiveValues<OakCombinedSpacingToken>;
   /**
    * Maximum number of characters
    */
@@ -26,6 +24,11 @@ export type OakMultilineTextProps = {
    */
   placeholder?: string;
   disabled: boolean;
+  ariaLabel?: string;
+  invalid?: boolean;
+  invalidText?: string;
+  value?: string;
+  onChange?: (value: string) => void;
 };
 
 type StyledOakTextAreaProps = {
@@ -33,16 +36,6 @@ type StyledOakTextAreaProps = {
 } & OakTextAreaProps;
 
 const StyledOakTextArea = styled(OakTextArea)<StyledOakTextAreaProps>`
-  background: ${parseColor("bg-primary")};
-  color: ${parseColor("text-subdued")};
-  border-radius: ${parseBorderRadius("border-radius-m")};
-  border-width: ${parseBorderWidth("border-solid-m")};
-  padding: ${parseSpacing("inner-padding-s")};
-  margin-left: ${parseSpacing("space-between-xs")};
-  margin-right: ${parseSpacing("space-between-xs")};
-  width: 100%;
-  border-color: ${parseColor("border-neutral-lighter")};
-
   &:focus-visible {
     border-color: ${(props) =>
       props.isError ? parseColor("red") : parseColor("border-primary")};
@@ -55,45 +48,80 @@ const StyledOakTextArea = styled(OakTextArea)<StyledOakTextAreaProps>`
   }
 `;
 
-const UnstyledComponent = (props: OakMultilineTextProps) => {
-  const [charCount, setCharCount] = useState(Number);
-  const [showCharCount, setShowCharCount] = useState(Boolean);
-  const [text, setText] = useState(String);
+const UnstyledComponent = forwardRef(
+  (
+    {
+      charLimit,
+      placeholder,
+      disabled,
+      invalid,
+      invalidText,
+      ariaLabel,
+      value,
+      onChange,
+    }: OakMultilineTextProps,
+    ref?: React.Ref<HTMLTextAreaElement>,
+  ) => {
+    const [charCount, setCharCount] = useState(Number);
+    const [showCharCount, setShowCharCount] = useState(Boolean);
 
-  const handleFocus = () => {
-    setShowCharCount(true);
-  };
+    const handleFocus = () => {
+      setShowCharCount(true);
+    };
 
-  const handleBlur = () => {
-    setShowCharCount(false);
-  };
+    const handleBlur = () => {
+      setShowCharCount(false);
+    };
 
-  const handleChange = (value: SetStateAction<string>) => {
-    const charCount = value.length;
-    setText(value);
-    setCharCount(charCount);
-  };
+    const handleChange = (value: string) => {
+      onChange && onChange(value);
+      const charCount = value.length;
+      setCharCount(charCount);
+    };
 
-  return (
-    <OakFlex $flexDirection={["row", "column"]}>
-      <StyledOakTextArea
-        value={text}
-        onFocus={handleFocus}
-        onChange={(e) => handleChange(e.target.value)}
-        onBlur={handleBlur}
-        maxLength={props.charLimit}
-        $height={["all-spacing-19", "all-spacing-13", "all-spacing-10"]}
-        placeholder={props.placeholder}
-        disabled={props.disabled}
-      ></StyledOakTextArea>
-      {showCharCount && (
-        <OakSpan $textAlign={"right"}>
-          {charCount}/{props.charLimit}
-        </OakSpan>
-      )}
-    </OakFlex>
-  );
-};
+    return (
+      <OakFlex
+        $flexDirection={["row", "column"]}
+        $gap={["space-between-xs", null]}
+      >
+        <StyledOakTextArea
+          ref={ref}
+          value={value}
+          onFocus={handleFocus}
+          onChange={(e) => handleChange(e.target.value)}
+          onBlur={handleBlur}
+          maxLength={charLimit}
+          placeholder={placeholder}
+          disabled={disabled}
+          $height={["all-spacing-19", "all-spacing-13", "all-spacing-10"]}
+          aria-label={ariaLabel}
+          $background="bg-primary"
+          $color={"text-subdued"}
+          $borderRadius={"border-radius-m"}
+          $ba={"border-solid-m"}
+          $pa={"inner-padding-s"}
+          $width="100%"
+          $borderColor={"border-neutral-lighter"}
+        ></StyledOakTextArea>
+        {showCharCount && (
+          <OakSpan
+            $textAlign={"right"}
+            aria-label="character count"
+            $font={"body-3"}
+            $color={"grey60"}
+          >
+            {charCount}/{charLimit}
+          </OakSpan>
+        )}
+        {invalid && invalidText && (
+          <OakP $font={"body-2"} $color={"text-error"}>
+            {invalidText}
+          </OakP>
+        )}
+      </OakFlex>
+    );
+  },
+);
 
 /**
  *
@@ -105,4 +133,4 @@ const UnstyledComponent = (props: OakMultilineTextProps) => {
  *
  * NB. We must export a styled component for it to be inheritable
  */
-export const OakMultilineText = styled(UnstyledComponent)``;
+export const OakMultilineText = UnstyledComponent;
