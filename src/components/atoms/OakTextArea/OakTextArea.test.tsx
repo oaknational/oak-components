4d7b79ccd01a4a5d97e207b637e536cfbbe40f76
Jessica Ababio <jessicaababio12@gmail.com>
2025-09-25 10:22:13 +0100

    feat: add allowCarriageReturn test

--- src/components/atoms/OakTextArea/OakTextArea.test.tsx
+++ src/components/atoms/OakTextArea/OakTextArea.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
 import { OakTextArea } from "./OakTextArea";
 
@@ -21,4 +22,33 @@ describe("OakTextArea", () => {
     const { container } = renderWithTheme(<OakTextArea />);
     expect(container).toMatchSnapshot();
   });
+
+  it.each([
+    [true, "test\ntest 2"],
+    [false, "testtest 2"],
+  ])(
+    "allows carriage return only when true",
+    async (allowCarriageReturn, expectedValue) => {
+      const user = userEvent.setup();
+      const { getByRole } = renderWithTheme(
+        <OakTextArea
+          data-testid="test"
+          disabled={false}
+          placeholder="Start typing answer..."
+          allowCarriageReturn={allowCarriageReturn}
+        />,
+      );
+      const textArea = getByRole("textbox");
+      expect(textArea).toBeInTheDocument();
+
+      await user.click(textArea);
+      await user.type(textArea, "test");
+
+      expect(textArea).toHaveValue("test");
+
+      await user.type(textArea, "{Enter}");
+      await user.type(textArea, "test 2");
+      expect(textArea).toHaveValue(expectedValue);
+    },
+  );
 });
