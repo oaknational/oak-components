097c4e0290ba085f5a149ae2918d3c6015cc7154
Jessica Ababio <jessicaababio12@gmail.com>
2025-09-25 19:38:12 +0100

    feat: add unit tests for callbacks and component logic

--- src/components/molecules/OakMultilineText/OakMultilineText.test.tsx
+++ src/components/molecules/OakMultilineText/OakMultilineText.test.tsx
@@ -53,7 +53,7 @@ describe("OakMultilineText", () => {
     expect(getByLabelText("character count")).toBeInTheDocument();
   });
 
-  it("changing value changed the text", async () => {
+  it("updates text when value changed", async () => {
     const { getByDisplayValue } = renderWithTheme(
       <OakMultilineText
         charLimit={200}
@@ -65,9 +65,68 @@ describe("OakMultilineText", () => {
     const textArea = getByDisplayValue("Hello");
     expect(textArea).toBeInTheDocument();
   });
-});
 
-// tests for callbacks
-/**
- * initial value
- */
+  it("trims text when pasted text length exceeds character limit", async () => {
+    const user = userEvent.setup();
+
+    const { getByDisplayValue } = renderWithTheme(
+      <OakMultilineText
+        charLimit={5}
+        $height="all-spacing-10"
+        disabled={false}
+        value="Hello"
+      />,
+    );
+
+    const textArea = getByDisplayValue("Hello");
+    expect(textArea).toBeInTheDocument();
+
+    await user.paste("Hello, world!");
+    expect(textArea).toHaveValue("Hello");
+  });
+
+  it("counts characters correctly when text when text is edited", async () => {
+    const user = userEvent.setup();
+
+    const { getByRole, getByLabelText } = renderWithTheme(
+      <OakMultilineText
+        charLimit={100}
+        $height="all-spacing-10"
+        disabled={false}
+      />,
+    );
+
+    const textArea = getByRole("textbox");
+    await user.type(textArea, "Hello, world!");
+    const charCount = getByLabelText("character count");
+
+    await user.click(textArea);
+    expect(textArea).toHaveFocus();
+    expect(charCount).toHaveTextContent("13/100");
+
+    await user.type(textArea, "{Backspace}");
+    expect(textArea).toHaveValue("Hello, world");
+    await user.click(textArea);
+    expect(textArea).toHaveFocus();
+    expect(charCount).toHaveTextContent("12/100");
+  });
+
+  it("shows invalid text if invalid is true and invalid text has been set", () => {
+    const { getByRole, getByLabelText } = renderWithTheme(
+      <OakMultilineText
+        charLimit={100}
+        $height="all-spacing-10"
+        disabled={false}
+        invalid={true}
+        invalidText="Invalid text"
+      />,
+    );
+
+    const textArea = getByRole("textbox");
+    expect(textArea).toBeInTheDocument();
+
+    const invalidText = getByLabelText("invalid text message");
+    expect(invalidText).toBeInTheDocument();
+    expect(invalidText).toHaveTextContent("Invalid text");
+  });
+});
