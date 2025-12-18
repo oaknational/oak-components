286dd85c2934624ccc0416dfb17df412008d02d3
Jim Cresswell <jim.cresswell@gmail.com>
2025-12-18 16:39:03 +0000

    feat: reduce extensibility to minimal entry point

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,36 +1,10 @@
-import { StorybookConfig } from "@storybook/nextjs";
-import { config as dotenvConfig } from "dotenv";
-
-let host = process.env.NEXT_PUBLIC_OAK_ASSETS_HOST;
-let path = process.env.NEXT_PUBLIC_OAK_ASSETS_PATH;
-let cloudinaryCloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
-
-const isLocal = !process.env.VERCEL && !process.env.GITHUB_ACTION;
-
-// Envs missing. If we are local, attempt to load from .env file. Note: this is redundant once we update to latest Storybook.
-if (isLocal && (!host || !path || !cloudinaryCloudName)) {
-  console.info("Attempting to load envs from .env file");
-  dotenvConfig({ path: [".env", ".env.local"] });
-
-  // Re-assign envs
-  host = process.env.NEXT_PUBLIC_OAK_ASSETS_HOST;
-  path = process.env.NEXT_PUBLIC_OAK_ASSETS_PATH;
-  cloudinaryCloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
-}
-
-// Final check regardless of context.
-if (!host || !path || !cloudinaryCloudName) {
-  throw new Error(
-    `All envs are required. NEXT_PUBLIC_OAK_ASSETS_HOST: ${!!host}, NEXT_PUBLIC_OAK_ASSETS_PATH: ${!!path}, and NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: ${!!cloudinaryCloudName}`,
-  );
-}
-
-const config: StorybookConfig = {
-  env: (existingEnv) => ({
-    ...existingEnv,
-    NEXT_PUBLIC_OAK_ASSETS_HOST: host,
-    NEXT_PUBLIC_OAK_ASSETS_PATH: path,
-    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: cloudinaryCloudName,
+const config = {
+  env: (config) => ({
+    ...config,
+    NEXT_PUBLIC_OAK_ASSETS_HOST: process.env.NEXT_PUBLIC_OAK_ASSETS_HOST,
+    NEXT_PUBLIC_OAK_ASSETS_PATH: process.env.NEXT_PUBLIC_OAK_ASSETS_PATH,
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
+      process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   }),
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
