0302440ec6e3f2265e27fffc065ac945d8fb138f
Weronika Szalas <freelance.wszalas@thenational.academy>
2026-01-20 16:11:24 +0100

    fix: force build of export parse function

--- src/styles/helpers/index.ts
+++ src/styles/helpers/index.ts
@@ -3,8 +3,8 @@ export { parseColor } from "./parseColor";
 export { parseColorFilter } from "./parseColorFilter";
 export { parseDropShadow } from "./parseDropShadow";
 export { parseOpacity } from "./parseOpacity";
-export { parseSpacing } from "./parseSpacing";
 export { parseTransitions } from "./parseTransitions";
+export { parseSpacing } from "./parseSpacing";
 export {
   parseFontSize,
   parseFontWeight,
