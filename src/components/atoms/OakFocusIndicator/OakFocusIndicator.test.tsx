38d4d6a2170ecb7ea5e1d058a550d5aacdcb995d
Jamie Blair <info@orangemug.co.uk>
2026-01-21 16:37:26 +0000

    test: added snapshot test when all props set

--- src/components/atoms/OakFocusIndicator/OakFocusIndicator.test.tsx
+++ src/components/atoms/OakFocusIndicator/OakFocusIndicator.test.tsx
@@ -5,8 +5,21 @@ import { OakFocusIndicator } from "./OakFocusIndicator";
 import renderWithTheme from "@/test-helpers/renderWithTheme";
 
 describe("FocusIndicator", () => {
-  test("render", async () => {
+  test("default render", async () => {
     const { baseElement } = renderWithTheme(<OakFocusIndicator />);
+
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  test("with props set", async () => {
+    const { baseElement } = renderWithTheme(
+      <OakFocusIndicator
+        hoverBackground="pink"
+        dropShadow="drop-shadow-centered-grey"
+        hoverDropShadow="drop-shadow-centered-grey"
+        activeDropShadow="drop-shadow-none"
+      />,
+    );
 
     expect(baseElement).toMatchSnapshot();
   });
