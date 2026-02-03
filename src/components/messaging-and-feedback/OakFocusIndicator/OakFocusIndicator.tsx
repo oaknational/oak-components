5c2e344c7fa93f1ea3a03742a21d44452f8dde86
Cat Miller <cat.miller@thenational.academy>
2026-02-03 11:45:04 +0000

    fix: reorder focus indiciator behaviour and remove z-indexes

--- src/components/messaging-and-feedback/OakFocusIndicator/OakFocusIndicator.tsx
+++ src/components/messaging-and-feedback/OakFocusIndicator/OakFocusIndicator.tsx
@@ -8,6 +8,8 @@ import { parseBorderRadius } from "@/styles/helpers/parseBorderRadius";
 
 function isJSDOM() {
   return globalThis?.navigator?.userAgent?.includes("jsdom/");
+};
+
 const activeChildren = `a:active, button:active`;
 const hoverChildren = `a:hover, button:hover`;
 const focusVisibleChildren = `
@@ -29,45 +31,23 @@ export type OakFocusIndicatorProps = {
  * Wrap focusable components `<a/>`/`<button/>` and this will add focus styles then the inner element is focused
  */
 export const OakFocusIndicator = styled(OakBox)<OakFocusIndicatorProps>`
-  box-shadow: ${(props) =>
-    props.dropShadow ? parseDropShadow(props.dropShadow) : "none"};
+  box-shadow: ${(props) => props.dropShadow ? parseDropShadow(props.dropShadow) : "none"};
 
-  &:has(
-      a${isJSDOM() ? "" : ":focus-visible"},
-        button${isJSDOM() ? "" : ":focus-visible"}
-    ) {
-    ${responsiveStyle(
-      "border-radius",
-      (props) => props.$borderRadius,
-      parseBorderRadius,
-    )}
-    z-index: 2;
-    box-shadow: ${parseDropShadow("drop-shadow-centered-lemon")},
-      ${parseDropShadow("drop-shadow-centered-grey")};
+    &:has(${hoverChildren}) {
+    ${responsiveStyle("background-color", (props) => props.hoverBackground, parseColor)}
+    box-shadow: ${(props) => props.hoverDropShadow ? parseDropShadow(props.hoverDropShadow) : "none"};
+  }
+    
+  &:has(${focusVisibleChildren}) {
+    ${responsiveStyle("border-radius", (props) => props.$borderRadius, parseBorderRadius)}
+    box-shadow: ${focusShadow};
+
+    & ${focusVisibleChildren} {
+      outline: none;
+    }
   }
 
-  &:has(a:hover, button:hover),
-  &:has(button:hover:not(:active${isJSDOM() ? "" : ", :focus-visible"})) {
-    z-index: 1;
-    ${responsiveStyle(
-      "background-color",
-      (props) => props.hoverBackground,
-      parseColor,
-    )}
-  }
-
-  &:has(a:hover, button:hover) {
-    box-shadow: ${(props) =>
-      props.hoverDropShadow ? parseDropShadow(props.hoverDropShadow) : "none"};
-  }
-
-  &:has(a:active, button:active) {
-    z-index: 2;
-    box-shadow: ${(props) =>
-      props.activeDropShadow
-        ? parseDropShadow(props.activeDropShadow)
-        : `${parseDropShadow("drop-shadow-lemon")}, ${parseDropShadow(
-            "drop-shadow-grey",
-          )};`};
+  &:has(${activeChildren}) {
+    box-shadow: ${(props) => props.activeDropShadow ? parseDropShadow(props.activeDropShadow) : fallbackActiveShadow};
   }
 `;
