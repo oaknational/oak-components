bbf63f053c53eb3d8159373d50e940ca42b09203
Aswani-VE3 <Aswani.Divakaran@ve3.global>
2026-02-11 14:23:11 +0000

    feat: add a hook to check the device type

---
+++ src/hooks/useMediaQuery.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+import { Device, getMediaQuery } from "@/styles/utils/responsiveStyle";
+
+export const useMediaQuery = (device: Device) => {
+  const [matches, setMatches] = useState(false);
+
+  const query = getMediaQuery(device);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    window.addEventListener("resize", listener);
+    return () => window.removeEventListener("resize", listener);
+  }, [matches, query]);
+
+  return matches;
+};
