a39835b96dbd61a6a096e5cb348fec7b2241758e
ben <ben.protheroe@thenational.academy>
2026-02-19 12:21:42 +0000

    feat: toggle updates

--- src/components/buttons/OakSmallSecondaryToggleButton/OakSmallSecondaryToggleButton.test.tsx
+++ src/components/buttons/OakSmallSecondaryToggleButton/OakSmallSecondaryToggleButton.test.tsx
@@ -37,4 +37,31 @@ describe("OakSmallSecondaryToggleButton component", () => {
     const button = getByRole("button");
     expect(button).toHaveAttribute("aria-pressed", "false");
   });
+  it("applies style ovverrides when provided", () => {
+    const { getByRole } = renderWithTheme(
+      <OakSmallSecondaryToggleButton
+        toggleOn={true}
+        defaultBorderColorToggleOn="bg-btn-primary"
+        defaultBorderColorToggleOff="text-primary"
+        defaultBackgroundToggleOn="bg-btn-primary"
+        defaultBackgroundToggleOff="bg-btn-secondary"
+        defaultTextColorToggleOn="text-inverted"
+        defaultTextColorToggleOff="text-primary"
+        hoverBackgroundToggleOn="bg-btn-primary-hover"
+        hoverBackgroundToggleOff="bg-btn-secondary-hover"
+        hoverBorderColorToggleOn="bg-btn-primary-hover"
+        hoverBorderColorToggleOff="text-primary"
+        hoverTextColorToggleOn="text-inverted"
+        hoverTextColorToggleOff="text-primary"
+        disabledBackgroundToggleOn="bg-btn-primary-disabled"
+        disabledBackgroundToggleOff="bg-btn-secondary-disabled"
+        disabledBorderColorToggleOn="bg-btn-primary-disabled"
+        disabledBorderColorToggleOff="text-disabled"
+        disabledTextColorToggleOn="text-inverted"
+        disabledTextColorToggleOff="text-disabled"
+      />,
+    );
+    const button = getByRole("button");
+    expect(button).toHaveStyle(`background: rgb(34, 34, 34)`);
+  });
 });
