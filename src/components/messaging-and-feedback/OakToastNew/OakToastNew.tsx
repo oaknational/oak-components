021eab1c109d4cd3bc74f79f5bfde3c24bf65df8
Cat Miller <cat.miller@thenational.academy>
2026-03-27 12:45:10 +0000

    feat: add tests for oaktoastnew

--- src/components/messaging-and-feedback/OakToastNew/OakToastNew.tsx
+++ src/components/messaging-and-feedback/OakToastNew/OakToastNew.tsx
@@ -70,12 +70,13 @@ const SuccessIcon = (
       $borderRadius="border-radius-circle"
       $position="absolute"
     />
-    <OakIcon iconName="success" $width="spacing-32" $height="spacing-32" />
+    <OakIcon data-testid="oak-toast-success-icon" iconName="success" $width="spacing-32" $height="spacing-32" />
   </OakBox>
 );
 
 const ErrorIcon = (
   <OakIcon
+    data-testid="oak-toast-error-icon"
     iconName="warning"
     $colorFilter="text-inverted"
     $height="spacing-32"
