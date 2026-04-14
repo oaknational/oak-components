e363881b5c05061374b388a206a1d9a67cd1d058
Coup <mia.coupland@thenational.academy>
2026-04-14 08:31:42 +0100

    fix: add flex to OakTabs LI to ensure styles align

--- src/components/navigation/OakTabs/OakTabs.tsx
+++ src/components/navigation/OakTabs/OakTabs.tsx
@@ -86,6 +86,7 @@ export function OakTabs<T extends string>(props: Readonly<OakTabsProps<T>>) {
             $listStyle={"none"}
             $width={"100%"}
             $height={"100%"}
+            $display={"flex"}
             key={label}
           >
             <StyledTabButton
