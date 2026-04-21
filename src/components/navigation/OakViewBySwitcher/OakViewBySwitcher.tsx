74609cb65056cebc0aa3c5a753a23bae058fbb39
Kyle Huggins <91190841+k-huggs@users.noreply.github.com>
2026-04-21 19:18:47 +0100

    fix: amend label name on test

--- src/components/navigation/OakViewBySwitcher/OakViewBySwitcher.tsx
+++ src/components/navigation/OakViewBySwitcher/OakViewBySwitcher.tsx
@@ -66,7 +66,7 @@ export function OakViewBySwitcher(props: Readonly<OakViewBySwitcherProps>) {
   return (
     <OakFlex
       as="nav"
-      aria-labelledby="view-by-switcher"
+      aria-labelledby="view-by-switcher-label"
       $flexDirection={"column"}
       $alignItems={"flex-start"}
       $gap={"spacing-4"}
