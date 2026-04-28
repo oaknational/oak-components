f1847f935f21d9326c99a006504d4e8a74e0ce6a
Cat Miller <cat.miller@thenational.academy>
2026-04-28 16:56:28 +0100

    fix: tests

--- src/components/owa/OakSideMenuNavLink/OakSideMenuNavLink.test.tsx
+++ src/components/owa/OakSideMenuNavLink/OakSideMenuNavLink.test.tsx
@@ -4,6 +4,7 @@ import "@testing-library/jest-dom";
 import { OakSideMenuNavLink } from "./OakSideMenuNavLink";
 
 import renderWithTheme from "@/test-helpers/renderWithTheme";
+import { getBreakpoint } from "@/styles/utils/responsiveStyle";
 
 describe("OakSideMenuNavLink", () => {
   it("renders", () => {
@@ -38,5 +39,32 @@ describe("OakSideMenuNavLink", () => {
       />,
     );
     expect(container).toMatchSnapshot();
+  });
+
+  it("supports custom selected and hover styling props", () => {
+    const { getByTestId, getByText } = renderWithTheme(
+      <OakSideMenuNavLink
+        data-testid="test"
+        isSelected={true}
+        item={{
+          heading: "Test",
+          subheading: "Test Subheading",
+          href: "/test",
+        }}
+        hoverBorderColor="border-neutral"
+        selectedBackground="bg-neutral"
+        selectedHeadingFont="heading-7"
+        onClick={() => {
+          // Do nothing
+        }}
+      />,
+    );
+
+    expect(getByTestId("test")).toHaveStyleRule("background-color", "#f2f2f2");
+    expect(getByTestId("test")).toHaveStyleRule("border-color", "#808080", {
+      media: `(min-width: ${getBreakpoint("small")}px)`,
+      modifier: ":hover",
+    });
+    expect(getByText("Test")).toHaveStyleRule("font-weight", "600");
   });
 });
