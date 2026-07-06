7b6c3ac940688052ad9d934ca6e3e585cf86cdcd
Jamie Blair <info@orangemug.co.uk>
2026-07-06 12:51:46 +0100

    feat: align drop shadow with themes

--- src/styles/theme/dropShadow.ts
+++ src/styles/theme/dropShadow.ts
@@ -1,12 +1,12 @@
 export const oakDropShadowTokens = {
-  "drop-shadow-standard": "0 0.5rem 0.5rem rgba(92, 92, 92, 20%)",
-  "drop-shadow-lemon": `0.125rem 0.125rem 0 rgba(255, 229, 85, 100%)`,
-  "drop-shadow-wide-lemon": `0.25rem 0.25rem 0 rgba(255, 229, 85, 100%)`,
-  "drop-shadow-centered-lemon": `0 0 0 0.125rem rgba(255, 229, 85, 100%)`,
-  "drop-shadow-grey": "0.25rem 0.25rem 0 rgba(87, 87, 87, 100%)",
-  "drop-shadow-centered-grey": "0 0 0 0.3rem rgba(87, 87, 87, 100%)",
-  "drop-shadow-black": "0.063rem 0.125rem 0 rgba(0,0,0, 100%)",
-  "drop-shadow-centred-standard": `0 0 0.5rem rgba(92, 92, 92, 20%)`,
+  "drop-shadow-standard": "0 0.5rem 0.5rem",
+  "drop-shadow-lemon": `0.125rem 0.125rem 0`,
+  "drop-shadow-wide-lemon": `0.25rem 0.25rem 0`,
+  "drop-shadow-centered-lemon": `0 0 0 0.125rem`,
+  "drop-shadow-grey": "0.25rem 0.25rem 0",
+  "drop-shadow-centered-grey": "0 0 0 0.3rem",
+  "drop-shadow-black": "0.063rem 0.125rem 0",
+  "drop-shadow-centred-standard": `0 0 0.5rem`,
   "drop-shadow-none": `none`,
 };
 
