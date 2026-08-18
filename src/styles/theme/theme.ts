ef29612a6f92786aa5a7735682a37b31a6bf31b0
Natassja Blore <natassja.blore@thenational.academy>
2026-08-18 15:59:38 +0100

    fix: remove deprecated type

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,13 +1,6 @@
-import { ThemedStyledProps, DefaultTheme } from "styled-components";
-
 import { UiRoleMap } from "./color";
 
 export type OakTheme = {
   name: string;
   uiColors: UiRoleMap;
 };
-
-export type PropsWithTheme<Props = unknown> = ThemedStyledProps<
-  Props,
-  DefaultTheme
->;
