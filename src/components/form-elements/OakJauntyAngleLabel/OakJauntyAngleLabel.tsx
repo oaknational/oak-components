7a0404549433357fc9df6aae6a203a47250ed94e
Jamie Blair <info@orangemug.co.uk>
2026-09-14 11:51:44 +0100

    Revert "fix: added missing "required" to <OakJauntyAngleLabel/> required by OWA"
    This reverts commit 3eba796e99cbf520722236bd34c70b085671f6ab.

--- src/components/form-elements/OakJauntyAngleLabel/OakJauntyAngleLabel.tsx
+++ src/components/form-elements/OakJauntyAngleLabel/OakJauntyAngleLabel.tsx
@@ -4,9 +4,8 @@ import { OakBox } from "@/components/layout-and-structure/OakBox";
 
 export type OakJauntyAngleLabelProps = {
   label: string;
-  // The following should only used when as="label"
+  // Should only used when as="label"
   htmlFor?: string;
-  required?: boolean;
 } & ComponentProps<typeof OakBox>; // Adding div type for OakBox for aria features
 export const OakJauntyAngleLabel = (props: OakJauntyAngleLabelProps) => {
   const { label, ...oakBoxProps } = props;
